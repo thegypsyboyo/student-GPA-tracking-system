00aa1633cf9d2d04248a3fb849db256fd23a23f3
thegypsyboyo <lewisonyango9@gmail.com>
2024-01-09 17:15:13 +0300

    routing,auth,layout

---
+++ app/(root)/session-period/unit-registered/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = ({ props }: any) => {
+  return <div>{props.slug}</div>;
+};
+
+export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,28 @@
+/* eslint-disable camelcase */
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+
 import "./globals.css";
+import { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "Student-GPA_tracking-system",
+  description:
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, vel!",
+  icons: "/assets/images/site-logo.svg",
+};
 
 export default function RootLayout({
   children,
@@ -8,9 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={` ${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
